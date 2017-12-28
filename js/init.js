7768d811571d1ac3ee14d36ed05f310b7f6a5312
Lesia Lametta <lesialametta@gmail.com>
2017-12-28 11:09:01 +0300

    Implementation of Owl Next and Prev Buttons to the sliders

--- js/init.js
+++ js/init.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
-  	items:1
+    items: 1,
+    nav: true,
+    navText: ['<i class="material-icons">keyboard_arrow_left</i>','<i class="material-icons">keyboard_arrow_right</i>']
   });
 });
